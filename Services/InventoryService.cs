a9808d9c0e91cd822a96c9f90a2dba1654f9f639
Anh Tuan <tuanlv@itsol.vn>
2022-08-27 17:26:23 +0700

    Init project

---
+++ Services/InventoryService.cs
@@ -0,0 +1,30 @@
+﻿namespace Inventory.Grpc.Services;
+
+using Inventory.Grpc.Protos;
+using Inventory.Grpc.Repositories;
+using System.Threading.Tasks;
+using ILogger = Serilog.ILogger;
+
+public class InventoryService : StockProtoService.StockProtoServiceBase
+{
+    private readonly InventoryRepository _inventoryRepository;
+    private readonly ILogger _logger;
+    public InventoryService(InventoryRepository inventoryRepository, ILogger logger)
+    {
+        _inventoryRepository = inventoryRepository ?? throw new ArgumentNullException(nameof(inventoryRepository));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+
+    public override async Task<StockModel> GetStock(GetStockRequest request, global::Grpc.Core.ServerCallContext context)
+    {
+        _logger.Information($"BEGIN Get Stock of ItemNo : {request.ItemNo}");
+        var stockQuantity = await _inventoryRepository.GetStockQuantity(request.ItemNo);
+        var result = new StockModel()
+        {
+            Quantity = stockQuantity
+        };
+
+        _logger.Information($"END Get Stock of ItemNo : {request.ItemNo} quantity: {result.Quantity}");
+        return result;
+    }
+}
